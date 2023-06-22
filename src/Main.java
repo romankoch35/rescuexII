5aaa64ada41f43184ce572a3d4151f93cd0cf0dd
romankoch35 <Kochetkov81@yandex.ru>
2023-06-22 04:43:26 +0300

    rescuex

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int initBalance = 1100;
+        int added = 3961;
+
+        int bonus = 0;
+        if (added > 1000); {
+            bonus = added / 100;
+        }
+        //  int bonus2 = added > 1000 ? added / 100 : 0; - или можно так, но у меня почему-то это криво сработало - решил оставить вот так
+
+        int total = initBalance + added + bonus;
+        int total2 = initBalance - added + bonus;
+
+        System.out.println("Ваш баланс: " + total + "рублей. "); // как быстро вводить эту команду, как на разборном вебинаре?
+        System.out.println("----------------------------"); // для красоты
+        System.out.println("Доступно бонусов: " + bonus + " рублей.");
+    }
+}
